f47a1c244b7086ce9ba35b42c07ecf025469b5bf
antekrtalic <antekrtalic2@gmail.com>
2021-04-20 13:10:21 +0200

    Calculating size of each small square dephending on number of it

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 const main_box = document.getElementById('boxes')
-main_box.style.height = '400px';
-main_box.style.width = '400px';
+main_box.style.height = '500px';
+main_box.style.width = '500px';
 
 let h_mainbox = main_box.offsetHeight;
 let w_mainbox = main_box.offsetWidth;
@@ -27,8 +27,12 @@ let number = askUser();
 /* Specification of small squares */ 
 
 x = number * number
-let h_square = h_mainbox / number;
-let w_square = w_mainbox / number;
+let h_square = (h_mainbox*w_mainbox) / x;
+h_square = Math.floor(Math.sqrt(h_square));
+let w_square = (h_mainbox*w_mainbox) / x;
+w_square = Math.floor(Math.sqrt(w_square));
+
+console.log(h_square);
 
 // Fill window with small divs
 for(let i = 0; i < x; i++){
@@ -36,9 +40,12 @@ for(let i = 0; i < x; i++){
     square.style.display = 'inline-block';
     square.style.border = '1px solid black';
     square.classList.add("little-square");
-    square.style.width = `${Math.sqrt(w_square)}px`;
-    square.style.height = `${Math.sqrt(h_square)}px`;
-    square.style.border = '2px solid red';
+    square.style.maxHeight = `100%`;
+    square.style.maxWidth = `100%`;
+    square.style.border = '1px solid black';
+    square.style.margin = "0px";
+    square.style.verticalAlign = 'top';
+    square.style.outline = 'none';
     
     main_box.appendChild(square);
     
