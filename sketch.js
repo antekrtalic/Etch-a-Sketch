ae9846d097ae9a3804889b81d9caa5ea54552fbc
antekrtalic <antekrtalic2@gmail.com>
2021-04-21 14:28:49 +0200

    Solved my problem with squares by implementing different style on each small square

--- sketch.js
+++ sketch.js
@@ -1,8 +1,7 @@
-const main_box = document.getElementById('boxes')
+const main_box = document.getElementById('boxes');
 main_box.style.height = '500px';
 main_box.style.width = '500px';
 
-let h_mainbox = main_box.offsetHeight;
 let w_mainbox = main_box.offsetWidth;
 
 /* Button event listener */
@@ -27,28 +26,24 @@ let number = askUser();
 /* Specification of small squares */ 
 
 x = number * number
-let h_square = (h_mainbox*w_mainbox) / x;
-h_square = Math.floor(Math.sqrt(h_square));
-let w_square = (h_mainbox*w_mainbox) / x;
-w_square = Math.sqrt(w_square);
+let w_square = (w_mainbox / number);
 
-console.log(h_square);
-console.log(w_square);
 
 // Fill window with small divs
 for(let i = 0; i < x; i++){
     let square = document.createElement('div');
-    square.style.display = 'inline-block';
+    
     square.style.boxSizing = 'border-box';
     square.style.border = '1px solid black';
     square.classList.add("little-square");
-    square.style.height = `${h_square}px`;
+    square.style.height = 'auto';
     square.style.width = `${w_square}px`;
-    square.style.border = '1px solid black';
-    square.style.margin = "0px";
-    square.style.verticalAlign = 'top';
-    //square.style.outline = 'none';
     
     main_box.appendChild(square);
     
 }
+
+// Changing colors on each square
+
+let duljina = document.getElementsByClassName('little-square').length;
+console.log(duljina);
